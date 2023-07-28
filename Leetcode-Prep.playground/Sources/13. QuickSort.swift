f48c99491bd1226882d50ce721e9cdeca1382557
Pavel Pakalnis <pavel.pokalnis@gmail.com>
2023-07-28 15:54:49 +0200

    Dev

--- Leetcode-Prep.playground/Sources/13. QuickSort.swift
+++ Leetcode-Prep.playground/Sources/13. QuickSort.swift
@@ -26,6 +26,56 @@ func insertionSort<Element>(_ array: inout [Element]) where Element: Comparable 
     }
 }
 
+// Merge Sort (split & merge) - average time complxity Θ(n log n), space compelxity O(n log n)
+func mergeSort<Element: Comparable>(_ array: [Element]) -> [Element] {
+    guard array.count > 1 else {
+        return array
+    }
+    let middle = array.count / 2
+    let left = mergeSort(Array(array[..<middle]))
+    let right = mergeSort(Array(array[middle...]))
+    return merge(left, right)
+}
+
+func merge<Element: Comparable>(_ left: [Element], _ right: [Element]) -> [Element] {
+    var leftIndex = 0, rightIndex = 0
+    var result: [Element] = []
+    while leftIndex < left.count && rightIndex < right.count {
+        let leftElement = left[leftIndex]
+        let rightElement = right[rightIndex]
+        if leftElement < rightElement {
+            result.append(leftElement)
+            leftIndex += 1
+        } else if rightElement < leftElement {
+            result.append(rightElement)
+            rightIndex += 1
+        } else {
+            result.append(leftElement)
+            leftIndex += 1
+            result.append(rightElement)
+            rightIndex += 1
+        }
+    }
+    if leftIndex < left.count {
+        result.append(contentsOf: left[leftIndex...])
+    }
+    if rightIndex < right.count {
+        result.append(contentsOf: right[rightIndex...])
+    }
+    return result
+}
+
+//Heap Sort
+extension Heap {
+    public mutating func sort() -> [Element] {
+        for index in elements.indices.reversed() {
+            elements.swapAt(0, index)
+            siftDown(from: 0, upTo: index)
+        }
+        return elements
+    }
+}
+
 // Quick Sort O(n log n)
 func quicksort<T: Comparable>(_ a: [T]) -> [T] {
   guard a.count > 1 else { return a }
@@ -37,3 +87,6 @@ func quicksort<T: Comparable>(_ a: [T]) -> [T] {
 
   return quicksort(less) + equal + quicksort(greater)
 }
+
+
+// Merge Sort O(n log n)
